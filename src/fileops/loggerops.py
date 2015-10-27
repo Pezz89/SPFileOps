24dd771cb9bbacefc93de8227889723f58881038
Sam Perry <samuel.perry89@gmail.com>
2015-10-27 16:32:55 +0000

    Modified logger formatting

--- src/fileops/loggerops.py
+++ src/fileops/loggerops.py
@@ -22,8 +22,9 @@ def create_logger(
     # messages
     logger.setLevel(logging.DEBUG)
 
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - '
-                                  '%(message)s')
+    formatter = logging.Formatter("[%(asctime)s] [%(levelname)8s] --- "
+                                  "%(message)s (%(filename)s:%(lineno)s)",
+                                  "%Y-%m-%d %H:%M:%S")
 
     # If a log file path has been provided...
     if log_filename:
